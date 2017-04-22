2e9792aeb26f1816349002ea85005ba7dfda73a8
sugus <jxffight@126.com>
2017-04-22 18:10:14 +0800

    读者信息
    修改读者信息从数据库获取

--- reader.php
+++ reader.php
@@ -53,20 +53,20 @@
 					<td width="11%" style="padding:3px;">修改</td>
 					<td width="11%" style="padding:3px;">删除</td>
 				</tr>
+				<?php 
+					include('conn/conn.php');
+					$infos=$pdo->query("select * from reader_info")->fetchAll(PDO::FETCH_ASSOC);
+					foreach ($infos as $info) {
+				 ?>
 				<tr align="center">
-					<td style="padding:5px;">郑莉</td>
-					<td style="padding:5px;">13902520001</td>
-					<td style="padding:5px;"><a href="#">3</a></td>
+					<td style="padding:5px;"><?php echo $info['name'] ?></td>
+					<td style="padding:5px;"><?php echo $info['tel'] ?></td>
+					<td style="padding:5px;"><a href="#"><?php echo $info['book_account'] ?></a></td>
 					<td style="padding:5px;"><a href="reader_modify.php">修改</a></td>
 					<td style="padding:5px;"><a href="#">删除</a></td>
 				</tr>
-				<tr align="center">
-					<td style="padding:5px;">曹阳</td>
-					<td style="padding:5px;">15276031282</td>
-					<td style="padding:5px;"><a href="#">7</a></td>
-					<td style="padding:5px;"><a href="#">修改</a></td>
-					<td style="padding:5px;"><a href="#">删除</a></td>
-				</tr>
+				<?php 
+				} ?>
 			</table>
 	</div>
 </body>
