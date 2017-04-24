902d73244fbfa5dd0c9b98e1b892b4997f481ae8
sugus <jxffight@126.com>
2017-04-24 13:40:14 +0800

    Update manager.php

--- manager.php
+++ manager.php
@@ -22,12 +22,7 @@
 	</style>
 </head>
 <body>
-<<<<<<< HEAD
 	<?php include('navigation.php'); ?>
-=======
-	<?php include("navigation.php");?>
-
->>>>>>> origin/master
 	<div class="content">
 		<div class="user">
 			<a href="book_add.php">添加图书</a>
